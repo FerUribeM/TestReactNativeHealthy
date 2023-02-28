7d9a3f405954f68aa643d28bd4ceb94442766c77
Fer Uribe <f.uribe@globant.com>
2023-02-28 15:26:04 -0600

    Call progress with token

--- android/app/src/main/java/com/awesomeproject/BeHealthyModule.java
+++ android/app/src/main/java/com/awesomeproject/BeHealthyModule.java
@@ -35,24 +35,25 @@ public class BeHealthyModule extends ReactContextBaseJavaModule {
         MainApplication mainApplication = (MainApplication) context;
         BeHealthyClient beHealthy = mainApplication.getBeHealthyClient();
         beHealthy.setEnvironment(BeHealthyEnvironment.STAGE);
-        beHealthy.setCommunityId("6");
-        beHealthy.setProgramName("Makasib");
-        beHealthy.supportFirebaseAnalytics(false);
-        beHealthy.setSupportColors(new SupportColors(
-                "FF9E1B",
-                "004C97",
-                "FF9E1B"));
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmLnVyaWJlQGdsb2JhbnQtb210bC10ZXN0IiwiZW1haWwiOiJmLnVyaWJlQGdsb2JhbnQtb210bC10ZXN0LmNvbSIsImxtc2lkIjoiZi51cmliZUBnbG9iYW50LW9tdGwtdGVzdCIsImV4cCI6MTY3Njg3NjEyMiwiaXNzIjoiT21hbnRlbCIsImlhdCI6MTY3NTg3NjEyMiwiYXVkIjoiQmVIZWFsdGh5In0.W0yaWG01uUX6mgyIO2ECymdhei9UTfUqQPUlEVhkvG8";
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            beHealthy.isUserEnrolled(token).whenCompleteAsync((isEnrolled, throwable) -> {
-                Timber.tag("isEnrolled").w("User isEnrolled = %s", isEnrolled);
-                if (isEnrolled) {
-                    beHealthy.startBeHealthy();
-                } else {
-                    beHealthy.startEnrollment(token);
-                }
-            });
-        }
+        beHealthy.setCommunityId("1");
+        //beHealthy.setProgramName("Makasib");
+        //beHealthy.supportFirebaseAnalytics(false);
+        //beHealthy.setSupportColors(new SupportColors(
+        //        "FF9E1B",
+        //        "004C97",
+        //        "FF9E1B"));
+        String token = "eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJmYWJlMDY2ZS1kMWE2LTRkMDMtOTlkZi1hMDI1N2VjOGYxNjgiLCJzdWIiOiI1NDU4MSIsImlzcyI6ImJlLWhlYWx0aHktYXV0aCIsImlkZW50aWZpY2F0aW9uIjoiYmVoZWFsdGh5c3RnYW5kQGdtYWlsLmNvbSIsImNvbW11bml0eUlkIjoxLCJhY2wiOnsicm9sZXMiOlsiVVNFUiJdfSwiaWF0IjoxNjc3NjE4Mjk3LCJleHAiOjE2Nzc2MjkwOTd9.fGulnN0NxQEeQUkHmkmLMhrOfUi3VPCn7No-4oxRTy36mw6iiP5AOQs1AvYgyvMpU6KfSqUkDpFrOwCsH3L8ig";
+        beHealthy.startProgress(token);
+        //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        //    beHealthy.isUserEnrolled(token).whenCompleteAsync((isEnrolled, throwable) -> {
+        //        Timber.tag("isEnrolled").w("User isEnrolled = %s", isEnrolled);
+        //        if (isEnrolled) {
+        //            beHealthy.startBeHealthy();
+        //        } else {
+        //            beHealthy.startEnrollment(token);
+        //        }
+        //    });
+        //}
     }
 
 }
